2c06b649cfbd18bcc57b616af330b309aea914e0
Stelios Karydas <stkarydas@gmail.com>
2024-09-11 10:57:24 +0300

    still changing fonts

--- app/app.py
+++ app/app.py
@@ -134,8 +134,8 @@ def recommendations_page():
                 if tmdb_response_details.status_code == 200:
 #                    st.write(f"**{movie}**")  # vanilla streamlit movie title
                     st.markdown(f'<p style="font-size:30px; text-decoration:underline;">**{movie}**</p>', unsafe_allow_html=True)
-#                    st.write(f"Overview: {tmdb_json.get('overview')}")   # vanilla streamlit overview
-                    st.markdown(f'<p style="font-size:30px; text-decoration:underline; background-color:black;">Overview: {tmdb_json.get('overview')}</p>', unsafe_allow_html=True)
+                    st.write(f"Overview: {tmdb_json.get('overview')}")   # vanilla streamlit overview
+#                    st.markdown(f'<p style="font-size:30px; text-decoration:underline; background-color:black;">Overview: {tmdb_json.get('overview')}</p>', unsafe_allow_html=True)
                     st.write(f"Runtime: {tmdb_json.get('runtime')} minutes")
 
                     genre_list_temp = tmdb_json.get('genres')
