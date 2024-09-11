85b5db141184b4d947b1ea71feafa3a9daf5e89b
Stelios Karydas <stkarydas@gmail.com>
2024-09-11 16:05:20 +0300

    various changes in frontend

--- app/app99.py
+++ app/app99.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import requests
-import random
 
 import os
 from dotenv import load_dotenv
+
+
 
 load_dotenv()
 
@@ -50,18 +51,18 @@ def input_page():
     # markdown for the welcome text
     st.markdown(
     """
-    ## Hello! Let me introduce myself, I am MRE, an AI powered Movie Recommendation Engine.
+    ## World's Best Recommendation Engine as rated by at least 3 Le Wagon students and their moms!
     """,
     unsafe_allow_html=True
     )
 
     # user input
-    user_id_input = st.number_input("And who might you be? (User ID)",
+    user_id_input = st.number_input("Please insert your User ID",
                                 min_value = 0,
                                 max_value = 2000,
                                 step = 1)
 
-    movie_suggestions = st.slider('How many movies would you like me to suggest?', 1, 15, 1)
+    movie_suggestions = st.slider('How many movies would you like MRE to suggest?', 1, 10, 1)
 
 #   i use this to make the go back button to work on the first try, but for some reason it bugs some times
 #   st.session_state.page = "recommendations"
@@ -73,47 +74,11 @@ def input_page():
         st.session_state.page = "recommendations"
 
 
-good_comments = [
-    "Yeah, that movie was good!",
-    "Awesome!",
-    "Loved it!",
-    "Hey, we should be friends!",
-    "Niiice!",
-    "I've seen better but hey, whatever floats your boat, amirite?",
-    "No way, that's my favorite!",
-    "OK!",
-    "Oh right, that's the one with that guy that starred in that other movie right?",
-    "Seriously?"
-]
-
-average_comments = [
-    "Meh, didn't like it either.",
-    "Yeah, no!",
-    "Pffft! Seen better...",
-    "Yeah, makes sense.",
-    "Aww, you didn't like that?"
-
-]
-
-# function commenting on user's ratings
-def comment(rating):
-    if rating >= 4.0:
-        return random.choice(good_comments)
-    elif rating > 1:
-        return random.choice(average_comments)
-    else:
-        return "Ouch!"
-
-
-
-
-
-
-
 ##### RECOMMENDATION PAGE #####
 
+
 def recommendations_page():
-    st.title("Alright, let me see what I can do for you...")
+    st.title("Let's see what MRE has prepared for you!")
 
     # retrieve user input from session state
     user_id = st.session_state.get("user_id", "N/A")
@@ -121,16 +86,11 @@ def recommendations_page():
 
     # API call to predict
     api_url = "https://movie-recommendation-engine-image-194144152402.europe-west1.run.app/predict"
-    params = {"user_id": int(user_id),
-               "top_n": int(num_recommendations)}
+    params = {"user_id": int(user_id), "top_n": int(num_recommendations)}
     response = requests.get(api_url, params=params)
 
     # TMDB API key
-    API_key = os.getenv("TMDB_API_KEY")       # this is for local use
-#    API_key = st.secrets["TMDB_API_KEY"]    # this is for the streamlit cloud app
-
-
-
+    API_key = st.secrets["TMDB_API_KEY"]    # os.getenv("TMDB_API_KEY")
 
     # CSS for improved layout and design
     st.markdown("""
@@ -165,71 +125,22 @@ def recommendations_page():
         border-radius: 5px;
     }
     .overview {
-        font-size: 18px;  /* Font size */
+        font-size: 14px;  /* Font size */
         margin-top: 10px;
         color: white;
     }
     .runtime, .genres, .score {
-        font-size: 14px;  /* Font size */
+        font-size: 12px;  /* Font size */
         margin-top: 5px;
         color: white;
     }
     </style>
     """, unsafe_allow_html=True)
 
-
-
-
-    # our predictions
+    # Our predictions
     if response.status_code == 200:
-        titles, tmdb_ids, history_titles, history_tmdb_ids, history_ratings = response.json()
-        # creating columns for the posters
-#        cols = st.columns(5)
+        titles, tmdb_ids = response.json()
 
-#       no need for this,it is just the table of predictions
-#        st.write(titles)
-
-
-# rating history of user
-        if len(history_titles) > 5:
-
-            st.markdown(
-            f"""
-            ### Your rating history looks interesting! You have rated {len(history_titles)} movies! Let's see some of them:
-            """,
-            unsafe_allow_html=True
-            )
-
-            for index in range(5):
-                st.write(f"You watched {history_titles[index]} and rated it {history_ratings[index]}/5.0.")
-                st.markdown(f'<p style="font-size:12px;">{comment(history_ratings[index])}</p>', unsafe_allow_html=True)
-
-            st.markdown('<p style="font-size:26px;">OK, great, I got you! According to these, here are my suggestions:</p>', unsafe_allow_html=True)
-
-        elif len(history_titles) > 1:
-
-            st.markdown(
-            f"""
-            ### You have only rated {len(history_titles)} movies! Let's see them:
-            """,
-            unsafe_allow_html=True
-            )
-            for index in range(len(history_titles)):
-                st.write(f"You watched {history_titles[index]} and rated it {history_ratings[index]}/5.0")
-
-            st.markdown("""<p style="font-size:24px;">Your rating history is rather small, but hey don't worry, I got you covered! Here are my suggestions:</p>""", unsafe_allow_html=True)
-#            st.write("Your rating history is rather small, but hey don't worry, I got you covered! Here are my suggestions:")
-
-        else:
-            st.markdown(
-            f"""
-            ### Aaah you must be new around here, you have not rated any movies! Alright, here are some suggestions to get you started:
-            """,
-            unsafe_allow_html=True
-            )
-
-
-# suggestions
         for idx, movie in enumerate(titles):
             # Create a transparent tile for each recommendation
             st.markdown(f'<div class="recommendation-tile">', unsafe_allow_html=True)
@@ -288,17 +199,18 @@ def recommendations_page():
 
             st.markdown('</div>', unsafe_allow_html=True)
 
-
-
     else:
-        st.write(f"OOPS! Something went wrong!Error code:{response.status_code}")
+        st.write(f"OOPS! Something went wrong! Error code: {response.status_code}")
 
     st.session_state.page = "input"
 
-    # button to go back
+    # Button to go back
     if st.button("Go back to user input"):
-        # store user input in session state and switch to recommendations page
         st.session_state.page = "input"
+
+
+
+
 
 
 
